3ea2089b67d36210e273f566c7d0c048d842ccf1
tomastoth <32282335+tomastoth@users.noreply.github.com>
2023-05-11 14:39:33 +0200

    add httputils (#2)
    * added mapper for user, resource for user, mvc test, added mockito and saving and finding of user
    
    * renamed postgres to postgres-dev and fixed param in UserResource
    
    * added Transactional to findUser in AppServiceImpl
    
    * added ExceptionAdvice, renamed UserResource to UserController
    
    * added basic security
    
    * added WalletAssetDto and WalletDto
    
    * extracted versions in pom.xml and added crypt-common dependency, added surefire for test split
    
    * added fetching of tokens from zapper, changed Asset to Token for clarity, moved common stuff to library crypto_common
    
    * added README
    
    * added HttpUtils

---
+++ src/main/java/xyz/kryom/wallets_backend/data_fetching/HttpUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Tomas Toth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package xyz.kryom.wallets_backend.data_fetching;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Tomas Toth
+ */
+public class HttpUtils {
+
+  private static final HttpClient client = HttpClient.newHttpClient();
+
+  public static CompletableFuture<String> fetchUrl(String url, Map<String, String> headers) {
+    HttpRequest.Builder httpRequestBuilder = HttpRequest.newBuilder(URI.create(url));
+    headers.forEach(httpRequestBuilder::header);
+    HttpRequest request = httpRequestBuilder.build();
+    return client.sendAsync(request, BodyHandlers.ofString())
+                 .thenApply(HttpResponse::body);
+  }
+}
